6f3e9053d209807233cc85681b27ec5fbe011b31
surlin.sxl@outlook.com <surlin.sxl@outlook.com>
2020-09-06 20:39:44 +0800

    定时任务

--- week10/main.py
+++ week10/main.py
@@ -2,17 +2,11 @@ import os
 import schedule
 from scrapy import cmdline
 
-# def job():
-#     d = os.path.dirname(__file__)
-#     abspath = os.path.abspath(d)
-#     print(abspath)
-#     scrapy_path = abspath+'/shopping'
-#     os.system('cd %s && scrapy crawl smzdm '% (scrapy_path) )
-#     os.system(r'python %s/DataCleaning/data_clean.py'% (abspath))  
-# schedule.every().day.at('02:00').do(job)
-
-d = os.path.dirname(__file__)
-abspath = os.path.abspath(d)
-print(abspath)
-scrapy_path = abspath+'/shopping'
-os.system('cd %s && scrapy crawl smzdm '% (scrapy_path) )
+def job():
+    d = os.path.dirname(__file__)
+    abspath = os.path.abspath(d)
+    print(abspath)
+    scrapy_path = abspath+'/shopping'
+    os.system('cd %s && scrapy crawl smzdm '% (scrapy_path) )
+    os.system(r'python %s/DataCleaning/data_clean.py'% (abspath))  
+schedule.every().day.at('02:00').do(job)
